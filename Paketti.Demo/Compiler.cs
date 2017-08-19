e028a214220f807ae1d326675d5e3df37d363ba9
Sir Phillip Tubell <sirphilliptubell@gmail.com>
2017-08-19 01:44:45 -0400

    Added demo project. Confirmed to work with VS 2017 15.1. Does not work with 15.3 or 15.3.1.

---
+++ Paketti.Demo/Compiler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Paketti.Contexts;
+using Paketti.Logging;
+using Paketti.Utilities;
+
+namespace Paketti.Cook
+{
+    /// <summary>
+    /// .Net Compiler.
+    /// </summary>
+    public class Compiler :
+        ICompiler
+    {
+        /// <summary>
+        /// Compiles the specified project.
+        /// </summary>
+        /// <param name="projectContext"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        public Result Compile(ProjectContext projectContext, ILog log)
+        {
+            using (log.LogStep("Compiling"))
+            {
+                var mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+                var systemLinq = MetadataReference.CreateFromFile(typeof(System.Linq.Enumerable).Assembly.Location);
+                var runtimeCompilerServices = MetadataReference.CreateFromFile(typeof(System.Runtime.CompilerServices.TupleElementNamesAttribute).Assembly.Location);
+                var runtime = MetadataReference.CreateFromFile(@"c:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Runtime.dll");
+                var references = projectContext.Project.MetadataReferences;
+
+                var compilation = CSharpCompilation.Create("TestCompilation", projectContext.DocumentSyntaxTrees, references, options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+                using (var outputStream = new MemoryStream())
+                {
+                    var emitResult = compilation.Emit(outputStream);
+
+                    if (emitResult.Success)
+                        return Result.Ok();
+                    else
+                    {
+                        var sb = new StringBuilder();
+                        foreach (var diagnostic in emitResult.Diagnostics)
+                        {
+                            if (sb.Length == 0)
+                                sb.Append(diagnostic);
+                            else
+                                sb.Append(Environment.NewLine).Append(diagnostic);
+                        }
+                        return Result.Fail(sb.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Paketti.Demo/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.Extensions.FileProviders;
+using Paketti.Library;
+using Paketti.Logging;
+using Paketti.Rewriters;
+using Paketti.Utilities;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Paketti.Cook
+{
+    internal class Program
+    {
+        const string solutionDrive = @"D:\";
+        const string solutionPath = @"Repositories\DotNetFunctional\DotNetFunctional.sln";
+
+        private static void Main(string[] args)
+        {
+            /* TODO
+             * Add InterfaceContext
+             * Add Delegates to Class/Struct/Interface Contexts
+             * Ensure Class/Struct/Interface Contexts's GetTypeDependencies() also check the types that they inherit from, eg: other classes/structs/etc..
+             *
+             * If the paketti package contains extension methods that do not use any interweaves. Then most likely the extension method is a helper for
+             * some interweave in the paketti project. In which case when a user imports that interweave, we should create an internal extension method class
+             * which contains these helper methods. The interweave may need to be re-written to explicitly use that extension method. We don't want to
+             * pollute the consumer's namespace with extension methods with ones that might collide with their own.
+             *
+             * Find and remove all debugger.break()s
+             * */
+             
+            Action<Project> display = DisplayProject;
+            
+            var compiler = new Compiler();
+            var log = new ConsoleLog();
+            var solutionFile = new PhysicalFileProvider(solutionDrive).GetFileInfo(solutionPath);
+            var solutionRewriter = new SolutionRewriter(log);
+            var builder = new SolutionToLibraryBuilder<MSBuildWorkspace>(compiler, solutionFile, MSBuildWorkspace.Create, solutionRewriter,
+                pc => new DependencyWalker(pc, log),
+                new PackageContentSelector(),
+                display,
+                log,
+                (ws, path) => ws.OpenSolutionAsync(path).Result);
+            var result = builder.Build();
+            
+            log.LogStep("Done");
+            Console.ReadKey();
+        }
+
+        private static void VerifySolutionOpens()
+        {
+            var ws = MSBuildWorkspace.Create();
+
+            ws.WorkspaceFailed += MSBuildWorkspace_WorkspaceFailed;
+
+            //In the event that assemblies are missing or nuget packages are incompatible, this will generally supply some error.
+            var solution = ws.OpenSolutionAsync(solutionDrive + solutionPath).Result;
+        }
+
+        private static void MSBuildWorkspace_WorkspaceFailed(object sender, WorkspaceDiagnosticEventArgs e)
+        {
+            Console.WriteLine(e.Diagnostic.Message);
+            Debugger.Break();
+        }
+
+        private static void DisplayProject(Project p)
+        {
+            var remainingDocuments = p
+                .Documents
+                .Select(x => new { Name = x.Name, ZContents = x.GetTextAsync().Result.ToString() })
+                .ToList();
+
+            Debugger.Break();
+        }
+    }
+}
